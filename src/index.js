fd663ab9c671e879444c0cd20bd80d44e36410f0
Nicolas Novello <nico.novello@gmail.com>
2018-06-26 16:44:10 +0200

    ok manage output

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import _ from 'lodash';
-import './scss/main.scss'
+import printMe from './print.js';
+import './scss/main.scss';
 
 function component() {
   var element = document.createElement('div');
+  var btn = document.createElement('button');
 
   // Lodash, now imported by this script
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
   element.classList.add('hello')
+
+  btn.innerHTML = 'Click me and check the console!';
+  btn.onclick = printMe
+
+  element.appendChild(btn);
 
   return element;
 }
