b129b5643dd454fc715e36f8b70b0caa9e41f4cb
Nicolas Novello <nico.novello@gmail.com>
2018-06-26 17:00:56 +0200

    hmr ok

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,16 +1,17 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const webpack = require('webpack')
 
 module.exports = {
   mode: 'development',
   entry: {
     app: './src/index.js',
-    print: './src/print.js'
   },
   devtool: 'inline-source-map',
   devServer: {
-    contentBase: './dist'
+    contentBase: './dist',
+    hot: true
   },
   output: {
     filename: '[name].bundle.js',
@@ -40,6 +41,7 @@ module.exports = {
     new CleanWebpackPlugin(['dist']),
     new HtmlWebpackPlugin({
       title: 'Output Management'
-    })
+    }),
+    new webpack.HotModuleReplacementPlugin()
   ]
 }
