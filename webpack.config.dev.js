a79f3cc472e025126d2042b862d1c108ce37fc16
Nicolas Novello <nico.novello@gmail.com>
2018-06-27 10:51:30 +0200

    clean project

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -2,7 +2,6 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const webpack = require('webpack')
 
 module.exports = {
